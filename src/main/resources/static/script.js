5ce1d5bd158a0aec7915f3e76f213d42ceafe610
bardlinga <balin6546@oslomet.no>
2024-02-06 14:38:49 +0100

    lagt til skallkode for funksjonar

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,34 @@
+<!--
+funksjonar:
+
+    declareBillettObjekt
+    definerer billettobjekt med nødvendige attributtar
+
+    opprettBillettArray
+    oppretter tomt array av typen billett
+
+    validerFilm
+    sjekkar at bruker har valgt noko anna enn "velg film"
+
+    validerNavn
+    sjekkar at navn er utfylt og berre inneheld bokstavar
+
+    validerNummer
+    sjekkar at nummer er fylt, er innanfor ein range og berre inneheld tall
+
+    validerEpost
+    sjekkar at epostfeltet er fylt og oppfyller krava til å være ein epost
+
+    opprettBillettObjekt
+    dersom alle inputs er gyldige, oppretter ein ny billett med verdiane fra inputs
+
+    leggTilBillettIArray
+    legger eit valgt billettobjekt til i bilettarrayet
+
+    visBillettArray
+    viser oppdatert array på sida
+
+    nullstillBillettArray
+    tømmer arrayet for alle objekt
+
+-->
