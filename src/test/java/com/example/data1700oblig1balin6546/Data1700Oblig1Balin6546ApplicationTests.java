dd00b8b0d63e942d4567858093f2ffdda6b73849
bardlinga <balin6546@oslomet.no>
2024-02-06 12:33:07 +0100

    init

---
+++ src/test/java/com/example/data1700oblig1balin6546/Data1700Oblig1Balin6546ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1balin6546;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig1Balin6546ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
